6102b374c92a3d782fca2436afe9cd2c091b2156
Dany Díaz <dany_diaz_22@hotmail.com>
2026-02-16 20:26:09 -0600

    completing some tests and adding coverage step

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -10,19 +10,33 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />);
+  screen.getByLabelText('New Item Name');
+});
 
-it.todo(
-  'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
-);
+it('has a "Add New Item" button that is disabled when the input is empty',() => {
+  render(<PackingList />);
+  expect(screen.getByLabelText('New Item Name')).toHaveValue('')
+  expect(screen.getByRole('button', { name: 'Add New Item'})).toBeDisabled()
+});
 
-it.todo(
-  'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
-);
+it('enables the "Add New Item" button when there is text in the input field', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByLabelText('New Item Name');
+  const addNewItemButton = screen.getByRole('button', { name: 'Add New Item'});
+  await user.type(newItemInput, 'Pencil')
+  expect(addNewItemButton).toBeEnabled()
+});
 
-it.todo(
-  'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
-);
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByLabelText('New Item Name');
+  const addNewItemButton = screen.getByRole('button', { name: 'Add New Item'});
+  const unpackedList = screen.getByTestId('unpacked-items-list')
+  expect(unpackedList.children).toHaveLength(0)
+  await user.type(newItemInput, 'Pencil')
+  await user.click(addNewItemButton)
+  expect(unpackedList.children).toHaveLength(1)
+  // expect(screen.getByLabelText('Pencil')).not.toBeChecked()
+});
